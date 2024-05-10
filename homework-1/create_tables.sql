465b2e163addcd308e216bf1c51ef46e12da368b
Denis <110720280+mor1nch@users.noreply.github.com>
2024-05-10 15:02:13 +0300

    hw1 done

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+    employee_id serial PRIMARY KEY,
+    first_name  varchar(50),
+    last_name   varchar(50),
+    title       varchar(255),
+    birth_date  varchar(50),
+    notes       text
+);
+
+CREATE TABLE customers
+(
+    customer_id  varchar(50) PRIMARY KEY,
+    company_name varchar(255),
+    contact_name varchar(255)
+);
+
+CREATE TABLE orders
+(
+    order_id    serial PRIMARY KEY,
+    customer_id varchar(50) REFERENCES customers (customer_id),
+    employee_id int  NOT NULL REFERENCES employees (employee_id),
+    order_date  date NOT NULL,
+    ship_city   varchar(255)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,49 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+connection = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345",
+    port="5432"
+)
+
+customers = "../homework-1/north_data/customers_data.csv"
+employees = "../homework-1/north_data/employees_data.csv"
+orders = "../homework-1/north_data/orders_data.csv"
+
+
+def create_tuple_of_values(values: list) -> tuple:
+    result = []
+    for value in values:
+        result.append(value)
+
+    return tuple(result)
+
+
+def create_sql_request(table_name, value) -> str:
+    brackets = "(" + '%s, ' * len(value)
+    brackets = brackets[:-2] + ")"
+    request = f"INSERT INTO {table_name} VALUES {brackets}"
+
+    return request
+
+
+def save_table_from_csv(conn, data_path: str, table_name: str) -> None:
+    with conn.cursor() as cur:
+        with open(data_path, 'r') as csv_file:
+            csv_reader = list(csv.reader(csv_file))
+
+            for i in range(1, len(list(csv_reader))):
+                values = create_tuple_of_values(csv_reader[i])
+                request = create_sql_request(table_name, values)
+
+                cur.execute(request, values)
+        conn.commit()
+
+
+if __name__ == '__main__':
+    save_table_from_csv(connection, orders, "orders")
